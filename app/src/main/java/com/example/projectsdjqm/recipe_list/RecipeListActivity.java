20b255b64a75ffb6e91cce3d7ac40e2d73b5fd74
Shahryar Malik <samalik@ualberta.ca>
2022-10-29 15:18:45 -0600

    Created Shopping List Class and RecipeListActivity class

---
+++ app/src/main/java/com/example/projectsdjqm/recipe_list/RecipeListActivity.java
@@ -0,0 +1,5 @@
+package com.example.projectsdjqm.recipe_list;
+
+public class RecipeListActivity {
+
+}
---
+++ app/src/main/java/com/example/projectsdjqm/shopping_list/ShoppingList.java
@@ -0,0 +1,5 @@
+package com.example.projectsdjqm.shopping_list;
+
+public class ShoppingList {
+
+}

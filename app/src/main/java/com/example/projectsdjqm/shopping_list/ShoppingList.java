a9850edd9b38694ff9ceec02b9f1281759fcbe69
Shahryar Malik <samalik@ualberta.ca>
2022-10-30 01:03:08 -0600

    Added variables and functions to ShoppingList.java

--- app/src/main/java/com/example/projectsdjqm/shopping_list/ShoppingList.java
+++ app/src/main/java/com/example/projectsdjqm/shopping_list/ShoppingList.java
@@ -1,5 +1,46 @@
+/**
+ * Classname: ShoppingList
+ *
+ * Version information: Version 1
+ *
+ * Date: 2022/09/29
+ */
+
 package com.example.projectsdjqm.shopping_list;
 
+import com.example.projectsdjqm.ingredient_storage.Ingredient;
+import java.util.ArrayList;
+
 public class ShoppingList {
+    /*
+     * Each ShoppingList class has the following:
+     * An Ingredient List
+     * Value to determine if ingredient has been picked up
+     */
+    private ArrayList<Ingredient> listOfIngredients;
+    private boolean pickedUp;
+
+
+    public ShoppingList(ArrayList<Ingredient> listOfIngredients, boolean pickedUp) {
+        this.listOfIngredients = listOfIngredients;
+        this.pickedUp = pickedUp;
+    }
+
+    /* Getters */
+    public ArrayList<Ingredient> getListOfIngredients() {return listOfIngredients;}
+
+    public boolean getPickedUp() {return pickedUp;}
+
+    /* Setters */
+    public void setListOfIngredients(ArrayList<Ingredient> listOfIngredients) {
+        this.listOfIngredients = listOfIngredients;
+    }
+
+    public void setPickedUp(boolean pickedUp) {
+        this.pickedUp = pickedUp;
+    }
+
+
+
 
 }

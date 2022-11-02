e6114cbcdd297baf400245633c9eb575d423cf69
Jianming Ma <jianming@ualberta.ca>
2022-11-01 18:02:15 -0600

    add meal plan class related stuff

--- app/src/main/java/com/example/projectsdjqm/meal_plan/MealplanActivity.java
+++ app/src/main/java/com/example/projectsdjqm/meal_plan/MealplanActivity.java
@@ -1,2 +1,69 @@
-package com.example.projectsdjqm.meal_plan;public class MealplanActivity {
+package com.example.projectsdjqm.meal_plan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.projectsdjqm.MainActivity;
+import com.example.projectsdjqm.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MealplanActivity extends AppCompatActivity implements MealPlanFragment.OnFragmentIteractionListener{
+
+    ListView mealplanlistview;
+    MealplanList mealplanAdapter;
+    ArrayList<Mealplan> mealplanlist;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mealplan_activity);
+
+        mealplanlistview = findViewById(R.id.meal_plan_list);
+
+        mealplanlist = new ArrayList<>();
+        mealplanAdapter = new MealplanList(this, mealplanlist);
+        mealplanlistview.setAdapter(mealplanAdapter);
+
+        // floating button for add meal plan
+        final FloatingActionButton addmealplanButton = findViewById(R.id.add_mealplan_button);
+        addmealplanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MealPlanFragment addmealplanFragment = new MealPlanFragment();
+                MealPlanFragment.show(getSupportFragmentManager(), "ADD_INGREDIENT");
+            }
+        });
+
+//        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.nav_view);
+//        navigation.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                switch (item.getItemId()) {
+//                    case R.id.navigation_home:
+//                        Intent a = new Intent(IngredientActivity.this, MainActivity.class);
+//                        startActivity(a);
+//                        break;
+//                }
+//                return false;
+//            }
+//        });
+
+    }
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/projectsdjqm/meal_plan/MealplanList.java
+++ app/src/main/java/com/example/projectsdjqm/meal_plan/MealplanList.java
@@ -1,2 +1,41 @@
-package com.example.projectsdjqm.meal_plan;public class MealplanList {
+package com.example.projectsdjqm.meal_plan;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.projectsdjqm.R;
+import com.example.projectsdjqm.ingredient_storage.Ingredient;
+import com.example.projectsdjqm.recipe_list.Recipe;
+
+import java.util.ArrayList;
+
+public class MealplanList extends ArrayAdapter<Mealplan> {
+
+    private ArrayList<Mealplan> mealplanList;
+    private Context context;
+
+
+    public MealplanList(Context context, ArrayList<Mealplan> mealplanList) {
+        super(context, 0, mealplanList);
+        this.mealplanList = mealplanList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.mealplan_content, parent, false);
+        }
+//        will implement the meal plan content depending on the recipe list here
+
+        return view;
+    }
 }

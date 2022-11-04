e2404a38749fcd127376bed9b688f4f3251b2c3d
Muchen Li <muchen@ualberta.ca>
2022-11-03 20:24:44 -0600

    connect ingrediet to database

--- app/src/main/java/com/example/projectsdjqm/ingredient_storage/IngredientActivity.java
+++ app/src/main/java/com/example/projectsdjqm/ingredient_storage/IngredientActivity.java
@@ -51,14 +51,12 @@ public class IngredientActivity extends AppCompatActivity implements
         IngredientList.IngredientButtonListener {
 
     BottomNavigationView bottomNavigationView;
-
     FirebaseFirestore db;
-
     final String TAG = "Ingredient Activity";
     Spinner spinner;
     public ListView ingredientlistview;
     IngredientList ingredientAdapter;
-    ArrayList<Ingredient> ingredientlist;
+    public ArrayList<Ingredient> ingredientlist;
     Ingredient selectedIngredient;
 
     @Override
--- app/src/main/java/com/example/projectsdjqm/recipe_list/RecipeListActivity.java
+++ app/src/main/java/com/example/projectsdjqm/recipe_list/RecipeListActivity.java
@@ -8,7 +8,9 @@ package com.example.projectsdjqm.recipe_list;
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -27,14 +29,22 @@ import com.example.projectsdjqm.ingredient_storage.IngredientFragment;
 import com.example.projectsdjqm.ingredient_storage.IngredientList;
 import com.example.projectsdjqm.meal_plan.MealPlanActivity;
 import com.example.projectsdjqm.shopping_list.ShoppingListActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.checkerframework.checker.units.qual.A;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 
 
 public class RecipeListActivity extends AppCompatActivity
@@ -42,16 +52,22 @@ public class RecipeListActivity extends AppCompatActivity
         RecipeFragment.OnFragmentInteractionListener{
 
     BottomNavigationView bottomNavigationView;
-
+    FirebaseFirestore db;
+    final String TAG = "Recipes Activity";
     ListView recipeListView;
     RecipeList recipeAdapter;
-    ArrayList<Recipe> recipeList;
+    public ArrayList<Recipe> recipeList;
+    Recipe selectedRecipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recipe_main);
 //        View view1 = LayoutInflater.from(this).inflate(R.layout.recipe_content, null);
+
+        Log.d(TAG, "onCreate");
+        db = FirebaseFirestore.getInstance();
+        CollectionReference collectionReference = db.collection("Recipes");
 
         bottomNavigationView = findViewById(R.id.nav_view);
         bottomNavigationView.setSelectedItemId(R.id.navigation_recipe_list);
@@ -94,18 +110,33 @@ public class RecipeListActivity extends AppCompatActivity
         recipeListView = findViewById(R.id.recipe_list);
         recipeList = new ArrayList<>();
         ArrayList<Ingredient> ingredientlist = new ArrayList<>();
-        ingredientlist.add(new Ingredient("egg",new Date(),Ingredient.Location.Pantry,3,2,"back"));
-        ingredientlist.add(new Ingredient("apple",new Date(2020,2,1),Ingredient.Location.Fridge,1,1,"here"));
-        ingredientlist.add(new Ingredient("ccc",new Date(2023,5,3),Ingredient.Location.Freezer,5,4,"ccc"));
+        //ingredientlist.add(new Ingredient("egg",new Date(),Ingredient.Location.Pantry,3,2,"back"));
+        //ingredientlist.add(new Ingredient("apple",new Date(2020,2,1),Ingredient.Location.Fridge,1,1,"here"));
+        //ingredientlist.add(new Ingredient("ccc",new Date(2023,5,3),Ingredient.Location.Freezer,5,4,"ccc"));
 
-        Drawable icon = ContextCompat.getDrawable(this, R.drawable.ic_notifications_black_24dp);
-        Recipe testa = new Recipe("Orange Chicken", "30", 3,
-                "category", "comments",icon,
-                ingredientlist);
-        recipeList.add(testa);
+        //Drawable icon = ContextCompat.getDrawable(this, R.drawable.ic_notifications_black_24dp);
+        //Recipe testa = new Recipe("Orange Chicken", "30", 3,
+        //        "category", "comments",icon,
+        //        ingredientlist);
+        //recipeList.add(testa);
         recipeAdapter = new RecipeList(this, recipeList);
         recipeAdapter.setRecipeButtonListener(this);
         recipeListView.setAdapter(recipeAdapter);
+
+        db.collection("Recipes")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
 
         final FloatingActionButton addButton = findViewById(R.id.add_recipe);
         addButton.setOnClickListener(new View.OnClickListener() {
@@ -125,12 +156,47 @@ public class RecipeListActivity extends AppCompatActivity
 
     // Delete button triggered
     public void onDeleteRecipeClickListener(int position) {
+        final CollectionReference collectionReference = db.collection("Ingredients");
+
+        selectedRecipe = recipeList.get(position);
         recipeList.remove(position);
         recipeAdapter.notifyDataSetChanged();
+        collectionReference
+                .document(selectedRecipe.getTitle())
+                .delete();
     }
 
     @Override
     public void onOkPressedAdd(Recipe recipe) {
+
+        final CollectionReference collectionReference = db.collection("Recipes");
+        final String recipeTitle = recipe.getTitle();
+        final String recipePreparationTime = recipe.getPreparationTime();
+        final int recipeServingNumber = recipe.getNumberofServings();
+        final String recipeCategory = recipe.getRecipeCategory();
+        final String recipeComments = recipe.getComments();
+        //final Drawable recipePhoto = recipe.getPhotograph();
+        final ArrayList<Ingredient> recipeIngredientList = recipe.getListofIngredients();
+
+        HashMap<String, Object> data = new HashMap<>();
+
+        data.put("Preparation Time", recipePreparationTime);
+        data.put("Serving Number", recipeServingNumber);
+        data.put("Category", recipeCategory);
+        data.put("Comments", recipeComments);
+        //data.put("Photo", recipePhoto);
+        data.put("Ingredient List",recipeIngredientList);
+
+        collectionReference
+                .document(recipeTitle)
+                .set(data)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, recipeTitle + " data has been added successfully!");
+                    }
+                });
+
         recipeAdapter.add(recipe);
     };
     public void onOkPressedEdit(Recipe recipe,

d651bd640d6ea62afae5fedf2824ff207c821393
Defrim Binakaj <74120374+DefrimBinakaj@users.noreply.github.com>
2022-11-16 10:55:49 -0700

    create firestore db class
    for avoiding db init repetition

---
+++ app/src/main/java/com/example/projectsdjqm/FireStoreDB.java
@@ -0,0 +1,17 @@
+/**
+ * FireStoreDB
+ * class used for firestore db inits to avoid repetition
+ * @version 1.0
+ * @author Defrim Binakaj
+ * @date Nov 16, 2022
+ */
+
+package com.example.projectsdjqm;
+
+
+public class FireStoreDB {
+  
+  FirebaseFirestore db;
+  // db = FirebaseFirestore.getInstance();
+  
+}

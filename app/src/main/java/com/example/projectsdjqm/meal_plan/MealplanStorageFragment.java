5314d323ad90a130f539b32fd8fb1bae1e1d1e5d
Muchen Li <muchen@ualberta.ca>
2022-11-25 12:56:33 -0700

    fix bug on meal plan page

--- app/src/main/java/com/example/projectsdjqm/meal_plan/MealplanStorageFragment.java
+++ app/src/main/java/com/example/projectsdjqm/meal_plan/MealplanStorageFragment.java
@@ -36,8 +36,6 @@ import com.example.projectsdjqm.recipe_list.RecipeList;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;

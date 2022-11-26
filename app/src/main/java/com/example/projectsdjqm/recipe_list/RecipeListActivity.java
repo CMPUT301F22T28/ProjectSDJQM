678b1d4d56f77f9e90b4fef74901dc4c0cbb8c96
Muchen Li <muchen@ualberta.ca>
2022-11-26 02:59:46 -0700

    update

--- app/src/main/java/com/example/projectsdjqm/recipe_list/RecipeListActivity.java
+++ app/src/main/java/com/example/projectsdjqm/recipe_list/RecipeListActivity.java
@@ -46,6 +46,7 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import java.util.ArrayList;

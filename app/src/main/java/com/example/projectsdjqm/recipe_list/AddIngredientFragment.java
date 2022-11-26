f21eb43c9db2b59a0f39d746983ab09817a3e771
qye <qye2022@gmail.com>
2022-11-26 11:08:31 -0700

    update adding ingredient to recipe - prevent invalid input

--- app/src/main/java/com/example/projectsdjqm/recipe_list/AddIngredientFragment.java
+++ app/src/main/java/com/example/projectsdjqm/recipe_list/AddIngredientFragment.java
@@ -5,8 +5,9 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
@@ -18,6 +19,10 @@ import com.example.projectsdjqm.ingredient_storage.Ingredient;
 
 public class AddIngredientFragment extends DialogFragment {
 
+    private EditText description;
+    private EditText category;
+    private EditText unitCost;
+    private EditText amount;
     private OnAddIngreidentFragmentIteractionListener listener;
 
 
@@ -31,42 +36,117 @@ public class AddIngredientFragment extends DialogFragment {
         if (context instanceof OnAddIngreidentFragmentIteractionListener) {
             listener = (OnAddIngreidentFragmentIteractionListener) context;
         } else  {
-            throw new RuntimeException(context.toString() + "This is not the correct fragment!");
+            throw new RuntimeException(context + "This is not the correct fragment!");
         }
     }
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        View view = LayoutInflater.from(getActivity()).inflate(R.layout.ingredient_in_recipe_content, null);
+        View view = getLayoutInflater().inflate(R.layout.ingredient_in_recipe_content, null);
 
-        EditText description = view.findViewById(R.id.description_l);
-        EditText category = view.findViewById(R.id.category_l);
-        EditText unitCost = view.findViewById(R.id.unit_cost_l);
-        EditText amount = view.findViewById(R.id.count_l);
+        description = view.findViewById(R.id.description_l);
+        category = view.findViewById(R.id.category_l);
+        unitCost = view.findViewById(R.id.unit_cost_l);
+        amount = view.findViewById(R.id.count_l);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        return builder
+        AlertDialog alertDialog = builder
                 .setView(view)
-                .setTitle("Add ingredient")
+                .setTitle("Add Ingredient")
                 .setNegativeButton("Cancel", null)
-                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        String descriptionInput = description.getText().toString();
-                        String categoryInput = category.getText().toString();
-                        int unitCostInput = Integer.parseInt(unitCost.getText().toString());
-                        int amountInput = Integer.parseInt(amount.getText().toString());
-                        Ingredient ingredient = new Ingredient(
-                                descriptionInput,
-                                null,
-                                null,
-                                amountInput,
-                                unitCostInput,
-                                categoryInput
-                                );
-                        listener.onAddIngredientOkPressed(ingredient);
+                .setPositiveButton("OK", null)
+                .create();
+        alertDialog.show();
 
+        Button okButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        okButton.setOnClickListener(new AddIngredientFragment.CustomListener(alertDialog));
+
+        return alertDialog;
+    }
+
+    class CustomListener implements View.OnClickListener {
+        private final Dialog dialog;
+
+        public CustomListener(Dialog dialog) {
+            this.dialog = dialog;
+        }
+
+        @Override
+        public void onClick(View v) {
+            String descriptionInput = description.getText().toString();
+            String categoryInput = category.getText().toString();
+            String unitCostInputStr = unitCost.getText().toString();
+            String amountInputStr = amount.getText().toString();
+            boolean isValid = true;
+
+            // check description input
+            if (descriptionInput.length() < 1) {
+                isValid = false;
+                description.setError("Enter a name");
+            } else if (descriptionInput.length() > Ingredient.MAX_LENGTH_NAME) {
+                description.setError("Name must be less than 30 characters");
+                isValid = false;
+            }
+
+            // check category input
+            if (categoryInput.length() < 1) {
+                isValid = false;
+                category.setError("Enter a name");
+            }
+
+            // check unit cost input
+            int unitCostInput = 0;
+            try {
+                if (!unitCostInputStr.isEmpty()) {
+                    unitCostInput = Integer.parseInt(unitCostInputStr);
+
+                    if (unitCostInput < 1) {
+                        isValid = false;
+                        unitCost.setError("Enter a positive number");
                     }
-                }).create();
+                } else {
+                    isValid = false;
+                    unitCost.setError("Enter a positive number");
+                }
+            } catch (NumberFormatException ex) {
+                isValid = false;
+                unitCost.setError("Enter a positive number");
+                Log.d("NumberFormatLog", "error on numberformat is " + ex.getMessage());
+                ex.printStackTrace();
+            }
+
+            // check amount input
+            int amountInput = 0;
+            try {
+                if (!amountInputStr.isEmpty()) {
+                    amountInput = Integer.parseInt(amountInputStr);
+
+                    if (amountInput < 1) {
+                        isValid = false;
+                        amount.setError("Enter a positive number");
+                    }
+                } else {
+                    isValid = false;
+                    amount.setError("Enter a positive number");
+                }
+            } catch (NumberFormatException ex) {
+                isValid = false;
+                amount.setError("Enter a positive number");
+                Log.d("NumberFormatLog", "error on numberformat is " + ex.getMessage());
+                ex.printStackTrace();
+            }
+            if (isValid) {
+                Ingredient ingredient = new Ingredient(
+                        descriptionInput,
+                        null,
+                        null,
+                        amountInput,
+                        unitCostInput,
+                        categoryInput
+                );
+                listener.onAddIngredientOkPressed(ingredient);
+                dialog.dismiss();
+            }
+        }
     }
 }

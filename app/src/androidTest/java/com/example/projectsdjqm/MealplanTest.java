57eb4af7d113f331a68ce8d1aaa83681b744d13b
W4LT3RM <jianming@ualberta.ca>
2022-11-27 23:36:06 -0700

    delete useless imports

---
+++ app/src/androidTest/java/com/example/projectsdjqm/MealplanTest.java
@@ -0,0 +1,92 @@
+package com.example.projectsdjqm;
+
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import android.app.Activity;
+
+
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.projectsdjqm.ingredient_storage.Ingredient;
+import com.example.projectsdjqm.ingredient_storage.IngredientActivity;
+import com.example.projectsdjqm.meal_plan.MealPlanActivity;
+import com.example.projectsdjqm.recipe_list.RecipeListActivity;
+import com.example.projectsdjqm.shopping_list.ShoppingListActivity;
+import com.robotium.solo.Solo;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.PickerActions.setDate;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+
+@RunWith(AndroidJUnit4.class)
+public class MealplanTest {
+
+    private Solo solo;
+
+    @Rule
+    public ActivityTestRule<MealPlanActivity> rule =
+            new ActivityTestRule<>(MealPlanActivity.class, true, true);
+
+    /**
+     * Runs before all tests and creates solo instance.
+     *
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+    }
+
+    /**
+     * Gets the Activity
+     *
+     * @throws Exception
+     */
+    @Test
+    public void start() throws Exception {
+        Activity activity = rule.getActivity();
+    }
+
+    @Test
+    public void check_add_mealplan() {
+        solo.assertCurrentActivity("Wrong Activity", MealPlanActivity.class );
+        onView(withId(R.id.add_meal_plan)).perform(click());
+    }
+}
